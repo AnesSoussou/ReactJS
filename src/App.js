0b651f33dea87a8e2387d7cd74ffb6bd96db4377
AnesSoussou <soussou.anes@gmail.com>
2021-01-19 13:01:05 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import {Button,Alert,Row,Col} from 'react-bootstrap';
+import Menu from './components/Menu';
+import LeftSide from './components/LeftSide';
+import RightSide from './components/RightSide';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
+      <Row className="landing">
+        <Col><LeftSide /></Col>
+        <Col><RightSide /></Col>
+      </Row>
     </div>
   );
 }
